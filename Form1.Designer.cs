4a88d75ec0d9e8234b1a5a91aa87b3b2b40825c7
Albert Richard Dammara <67355005+rdammara@users.noreply.github.com>
2023-08-04 00:58:10 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace KeyboardCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DisableKeyboardBtn = new CheckBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // DisableKeyboardBtn
+            // 
+            DisableKeyboardBtn.AutoSize = true;
+            DisableKeyboardBtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            DisableKeyboardBtn.Location = new Point(72, 159);
+            DisableKeyboardBtn.Name = "DisableKeyboardBtn";
+            DisableKeyboardBtn.Size = new Size(157, 25);
+            DisableKeyboardBtn.TabIndex = 0;
+            DisableKeyboardBtn.Text = "Lock My Keyboard";
+            DisableKeyboardBtn.UseVisualStyleBackColor = true;
+            DisableKeyboardBtn.CheckedChanged += DisableKeyboardBtn_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(36, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Keyboard Cleaner v1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(323, 290);
+            Controls.Add(label1);
+            Controls.Add(DisableKeyboardBtn);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox DisableKeyboardBtn;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeyboardCleaner
+{
+    public partial class Form1 : Form
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                if (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_KEYUP)
+                {
+                    if (!keyboardEnabled)
+                    {
+                        return (IntPtr)1; // Block the keyboard event
+                    }
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _proc = HookCallback;
+            _hookID = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, IntPtr.Zero, 0);
+        }
+
+        private bool keyboardEnabled = true;
+
+        private void DisableKeyboardBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            keyboardEnabled = !DisableKeyboardBtn.Checked;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (_hookID != IntPtr.Zero)
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+            base.OnFormClosing(e);
+        }
+    }
+
+
+}
+
+
